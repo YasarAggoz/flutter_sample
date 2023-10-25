53105e51ec268ec3c764e495c67c191ed9d6c633
YasarAggoz <aggozyasar@hotmail.com>
2023-10-25 11:09:05 +0300

    button navigation

--- lib/sekmeler/homepage.dart
+++ lib/sekmeler/homepage.dart
@@ -13,42 +13,122 @@ class _HomepageState extends State<Homapage> {
 
   @override
   Widget build(BuildContext context) {
-    int _currentindex = 0 ;
-    const colorizeColors = [
-      Colors.indigo,
-      Colors.blue,
-      Colors.black38,
-      Colors.blueGrey,
+    int _selectedIndex = 0;
+     const TextStyle optionStyle =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+     const List<Widget> _widgetOptions = <Widget>[
+      Text(
+        'Ana Sayfa',
+        style: optionStyle,
+      ),
+      Text(
+        'Tv',
+        style: optionStyle,
+      ),
+      Text(
+        'Login',
+        style: optionStyle,
+      ),
+      Text(
+        'Geçmiş',
+        style: optionStyle,
+      ),
     ];
-    const colorizeTextStyle = TextStyle(
-      fontSize: 27.0,
-      fontFamily: 'Smooch',
-    );
+
+    void _onItemTapped(int index) {
+      setState(() {
+        _selectedIndex = index;
+        if (_selectedIndex == 0)
+          {
+            Navigator.push (
+              context,
+              MaterialPageRoute(builder: (context) => const Homapage()),
+            );
+          }
+        if (_selectedIndex == 1)
+        {
+          Navigator.push (
+            context,
+            MaterialPageRoute(builder: (context) => const Homapage()),
+          );
+        }
+        if (_selectedIndex == 2)
+        {
+          Navigator.push (
+            context,
+            MaterialPageRoute(builder: (context) =>  userpage()),
+          );
+        }
+        if (_selectedIndex == 3)
+        {
+          Navigator.push (
+            context,
+            MaterialPageRoute(builder: (context) => const Homapage()),
+          );
+        }
+
+
+
+
+      });
+    }
     return Scaffold (
         appBar: AppBar(
           title: const Text('Ana Sayfa'),
         ),
-        body: Center(child: ElevatedButton(
-            style:
-            ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-            onPressed: () async {
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tv),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.border_color_outlined),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
 
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) {
-                return
-                   userpage();
-              }
-              ));
 
-            },
-            child: const Text(
-              "Üye Ol",
-              style: TextStyle(fontSize: 25),
-            )
-
-        ),
-
-        )
+        // child: Center(child: ElevatedButton(
+        //     style:
+        //     ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+        //     onPressed: () async {
+        //
+        //       Navigator.push(
+        //           context, MaterialPageRoute(builder: (context) {
+        //         return
+        //            userpage();
+        //       }
+        //       ));
+        //
+        //     },
+        //     child: const Text(
+        //       "Üye Ol",
+        //       style: TextStyle(fontSize: 25),
+        //     )
+        //
+        // ),
+        //
+        // )
     );
   }
 }
